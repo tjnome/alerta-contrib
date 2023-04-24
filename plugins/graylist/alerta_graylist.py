3bada080ec413cbfff27d84ddcd3155f828b5bfd
Terje Nomeland <terje.nomeland@basefarm.com>
2023-04-24 19:25:01 +0200

    fucking works

--- plugins/graylist/alerta_graylist.py
+++ plugins/graylist/alerta_graylist.py
@@ -37,12 +37,12 @@ class GrayHandler(PluginBase):
                 tags[tag] = None
 
         if not all([tag in tags for tag in REPORTER_TAGS]):
-            LOG.debug(f'Missing {REPORTER_TAGS} in alert: {alert}')
+            LOG.debug(f'[Graylist] Missing {REPORTER_TAGS} in alert: {alert}')
             return alert
 
         if (not tags.get('targethost') and tags.get('host') == tags['pamola_host']):
             LOG.debug(
-                f'host matches pamola_host in alert: {alert}')
+                f'[Graylist] host matches pamola_host in alert: {alert}')
 
             # Insert customerprefix, externalid.
             for key in CUSTOMER_TAGS:
@@ -52,28 +52,24 @@ class GrayHandler(PluginBase):
             for key in REPORTER_TAGS:
                 tags.pop(key, None)
 
-            alert.tags.clear()
-            for key, value in tags.items():
-                if value is None:
-                    alert.tags.append(key)
-                else:
-                    alert.tags.append(key + '=' + value)
-
+            alert.tags = self.dict_to_list(tags)
             return alert
 
         filters = Filter.find_matching_filters(alert, 'graylist')
         if not filters:
             # Impersonate (Amogus)
             LOG.warning(
-                f'Filter does not match alert. Amogus SUS alert: {alert} tags: {alert.tags}')
+                f'[Graylist] Filter does not match alert. Amogus SUS alert: {alert} tags: {alert.tags}')
             # Remove targethost
             tags.pop('targethost', None)
             for key in REPORTER_TAGS:
                 # Insert/Overrride host, externalid, customerprefix
-                tags[key.split('_', 1)[1], tags[key]]
+                tags[key.split('_', 1)[1]] = tags[key]
                 # Remove reporter tags
                 tags.pop(key, None)
-            LOG.debug(f'Return Amogus SUS alert object: {alert} tags: {alert.tags}')
+            LOG.debug(f'[Graylist] Return Amogus SUS alert object: {alert} tags: {alert.tags}')
+
+            alert.tags = self.dict_to_list(tags)
             return alert
 
         for f in filters:
@@ -81,16 +77,18 @@ class GrayHandler(PluginBase):
                 grayattr = GrayAttributes(**f.attributes)
             except Exception as e:
                 LOG.warning(
-                    f'filter has invalid attributes: {f.id} error: {e}')
+                    f'[Graylist] filter has invalid attributes: {f.id} error: {e}')
                 continue
 
             if grayattr.host == tags['pamola_host']:
                 if 'alert' in grayattr.roles:
                     for key in REPORTER_TAGS:
-                        alert.tags.remove(key + '=' + tags[key])
+                        tags.pop(key, None)
+                    
+                    alert.tags = self.dict_to_list(tags)
                     return alert
 
-        raise RejectException
+        raise RejectException(f"[Graylist] rejected alert '{alert}'")
 
     def post_receive(self, alert, **kwargs):
         return alert
@@ -111,89 +109,65 @@ class GrayHandler(PluginBase):
             if '=' in tag:
                 key, value = tag.split('=', 1)
                 tags[key] = value
+            else:
+                tags[tag] = None
 
         if not all([tag in tags for tag in REPORTER_TAGS]):
-            LOG.debug(f'Missing {REPORTER_TAGS} in blackout: {blackout}')
+            LOG.debug(f'[Graylist] Missing {REPORTER_TAGS} in blackout: {blackout}')
             return blackout
 
         if any(tag.startswith(tags['pamola_host']) for tag in blackout.tags):
             if not tags.get('targethost'):
-                if not tags.get('externalid') and not tags.get('customerprefix'):
-                    # Insert customerprefix, externalid.
-                    for key in CUSTOMER_TAGS:
-                        blackout.tags.append(
-                            key + '=' + tags['pamola' + '_' + key])
+                for key in CUSTOMER_TAGS:
+                    tags[key] = tags['pamola' + '_' + key]
+                
+                for key in REPORTER_TAGS:
+                    tags.pop(key, None)
 
-                    # Remove REPORTER_TAGS
-                    for key in REPORTER_TAGS:
-                        blackout.tags.remove(key + '=' + tags[key])
-                    return blackout
-
-                if (tags.get('externalid') == tags['pamola_externalid'] and tags.get('customerprefix') == tags['pamola_customerprefix']):
-                    # Remove REPORTER_TAGS
-                    for key in REPORTER_TAGS:
-                        blackout.tags.remove(key + '=' + tags[key])
-                    return blackout
+                blackout.tags = self.dict_to_list(tags)
+                return blackout
 
         # Check if blackout matches anything:
-        filters = Filter.find_matching_filters(
-            self.parse(blackout), 'graylist')
+        filters = Filter.find_matching_filters(blackout, 'graylist')
 
         if not filters:
-            raise RejectException
+            raise RejectException(f"[Graylist] rejected blackout '{blackout}'")
 
         for f in filters:
             try:
                 grayattr = GrayAttributes(**f.attributes)
             except Exception as e:
                 LOG.warning(
-                    f'filter has invalid attributes: {f.id} error: {e}')
+                    f'[Graylist] filter has invalid attributes: {f.id} error: {e}')
                 continue
 
             if grayattr.host == tags['pamola_host']:
                 if 'blackout' in grayattr.roles:
                     for key in REPORTER_TAGS:
-                        blackout.tags.remove(key + '=' + tags[key])
+                        tags.pop(key, None)              
+                    blackout.tags = self.dict_to_list(tags)
                     return blackout
 
-        raise RejectException
+        raise RejectException(f"[Graylist] rejected blackout '{blackout}'")
 
     def delete_blackout(self, blackout: 'Blackout', **kwargs) -> bool:
-        LOG.debug(f'Blackout that is deleted: {blackout}')
         raise NotImplementedError
 
     def create_filter(self, filter: 'Filter', **kwargs) -> 'Filter':
-        LOG.debug(f'Filter that is used: {filter}')
         raise NotImplementedError
 
     def receive_filter(self, filter: 'Filter', **kwargs) -> 'Filter':
-        LOG.debug(f'Filter that it matches: {filter}')
         raise NotImplementedError
 
     def delete_filter(self, filter: 'Filter', **kwargs) -> bool:
-        LOG.debug(f'Filter that is deleted: {filter}')
         raise NotImplementedError
 
     @staticmethod
-    def parse(blackout: 'Blackout') -> 'Alert':
-        return Alert(
-            id=None,
-            resource=blackout.get('resource', None),
-            event=blackout.get('event', None),
-            environment=blackout.get('environment', None),
-            severity=None,
-            correlate=list(),
-            status=None,
-            service=blackout.get('service', list()),
-            group=blackout.get('group', None),
-            value=None,
-            text=None,
-            tags=blackout.get('tags', list()),
-            attributes=dict(),
-            origin=None,
-            event_type=None,
-            create_time=None,
-            timeout=None,
-            raw_data=None,
-            customer=None
-        )
+    def dict_to_list(tags: dict) -> list:
+        tags_list = []
+        for key, value in tags.items():
+            if value is None:
+                tags_list.append(key)
+            else:
+                tags_list.append(key + '=' + value)
+        return tags_list
